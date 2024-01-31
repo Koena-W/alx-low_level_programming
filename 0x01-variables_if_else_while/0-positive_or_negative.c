7058b7f06eb533c6077cf89ffe84e69e54e17d50
Koena <kowena780@gmail.com>
2024-01-31 09:52:23 -0800

    Program to determine negative, zero or positive number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/****************************************************
- * main - check the code
+/* more headers goes there */
+/**
+ * main - Entry Point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  **/
 int main(void)
 {
