6e321366a29b840433e39d126456fe72fe5ac714
Koena <kowena780@gmail.com>
2024-02-01 05:33:21 -0800

    Code to print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - code goes here
+ *
+ * Code to print numbers from 0 to 9 followed by new ling
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		putchar('\n');
+	}
+	return (0);
+}
