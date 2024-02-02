3266af568ffdb60e6b507f393f5b369265289da0
Koena <kowena780@gmail.com>
2024-02-02 05:16:49 -0800

    Combination of 3 numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - my code goes here
+ *
+ * Code for possible 3 combinations
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int i, n, m;
+
+	for (m = 0; m < 8; m++)
+	{
+		for (n = m + 1; n < 9; n++)
+		{
+			i = n + 1;
+			do {
+				putchar('0' + m);
+				putchar('0' + n);
+				putchar('0' + i);
+				if (m < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				i++;
+			} while (i < 10);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
